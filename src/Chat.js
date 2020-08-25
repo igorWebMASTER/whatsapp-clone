5ed24f70333a7afb8d4904d06d2b79c238f08de9
igorWebMASTER <igor_1917@hotmail.com>
2020-08-24 23:55:04 -0300

    aplication layout

---
+++ src/Chat.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import "./Chat.css";
+import { Avatar, IconButton, Icon } from "@material-ui/core";
+
+import { SearchOutlined, MoreVert, AttachFile } from "@material-ui/icons";
+
+function Chat() {
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p> last Seen at..</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">Hey Guys</p>
+      </div>
+      <div className="chat__footer"></div>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCvny5dhnoGuPotrsX1G6LeWRScH8brpss",
+  authDomain: "whatsapp-clone-6e25a.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-6e25a.firebaseio.com",
+  projectId: "whatsapp-clone-6e25a",
+  storageBucket: "whatsapp-clone-6e25a.appspot.com",
+  messagingSenderId: "230370405751",
+  appId: "1:230370405751:web:e7edbbab27dd3ccac28572",
+  measurementId: "G-0FPPRRX0D0",
+};
